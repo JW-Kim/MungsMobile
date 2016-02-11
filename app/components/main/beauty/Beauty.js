e4b5fecd8cd327bc2aa7b3082d030bdd17c78c06
JW-Kim <jw4005.kim@gmail.com>
2016-02-12 05:51:38 +0900

    ajax call 연동

--- app/components/main/beauty/Beauty.js
+++ app/components/main/beauty/Beauty.js
@@ -1,45 +1,116 @@
 'use strict';
 
 var React = require('react-native');
-var {StyleSheet, ScrollView, View, Text} = React;
+var {StyleSheet, ScrollView, View, Text, ListView, Image} = React;
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
 
 class Beauty extends React.Component {
 
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false,
+        };
+    };
+
+    componentDidMount() {
+        this.fetchData();
+    };
+
+    fetchData() {
+        fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseData) => {
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+                loaded: true,
+            });
+        })
+        .done();
     };
 
     render(){
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+
         return (
-            <ScrollView tabLabel="ion|ios-paper" style={styles.tabView}>
-                <View style={styles.card}>
-                <Text>미용</Text>
-                </View>
-            </ScrollView>
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderMovie}
+                style={styles.listView}
+            />
         );
-    }
+    };
+
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies...
+                </Text>
+            </View>
+        );
+    };
+
+    renderMovie(movie) {
+        return (
+            <View style={styles.container}>
+                <Image
+                source={{uri: movie.posters.thumbnail}}
+                style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    };
+
 }
 
 var styles = StyleSheet.create({
 
-  tabView: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: 'rgba(0,0,0,0.01)',
-  }
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 
-  ,card: {
-      borderWidth: 1,
-      backgroundColor: '#fff',
-      borderColor: 'rgba(0,0,0,0.1)',
-      margin: 5,
-      height: 150,
-      padding: 15,
-      shadowColor: '#ccc',
-      shadowOffset: {width: 2, height: 2},
-      shadowOpacity: 0.5,
-      shadowRadius: 3,
-    }
+    rightContainer: {
+        flex: 1,
+    },
+
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+
+    year: {
+       textAlign: 'center',
+    },
+
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+
+    listView: {
+        backgroundColor: '#F5FCFF',
+    },
 })
 
 module.exports = Beauty;
