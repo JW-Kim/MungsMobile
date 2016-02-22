d38b96c6daeb4ef475f1239f0e838c379d360827
JW-Kim <jw4005.kim@gmail.com>
2016-02-23 00:57:23 +0900

    modal 처리

--- app/components/main/beauty/BeautyRequests.js
+++ app/components/main/beauty/BeautyRequests.js
@@ -2,13 +2,8 @@
 
 var React = require('react-native');
 var Actions = require('react-native-router-flux').Actions;
-var {View, Text, StyleSheet} = React;
-
-import DropDown, {
-    Select,
-    Option,
-    OptionList,
-} from 'react-native-selectme';
+var {View, Text, StyleSheet, TouchableHighlight} = React;
+var Modal   = require('react-native-modalbox');
 
 class BeautyRequests extends React.Component {
 
@@ -16,26 +11,17 @@ class BeautyRequests extends React.Component {
         super(props);
 
         this.state = {
-            canada: ''
+
         };
     }
 
-    _getOptionList() {
-        return this.refs['OPTIONLIST'];
-    }
-
-
-    _canada(province) {
-
-        this.setState({
-            ...this.state,
-            canada: province
-        });
+    openTypeModal() {
+        this.refs.typeModal.open();
     }
 
     render(){
         return(
-            <View>
+            <View style={{flex: 1}}>
                 <View style={styles.headerContainer}>
                     <Text style={styles.back} onPress={Actions.pop}>뒤로</Text>
                     <Text style={styles.title}>미용 요청서</Text>
@@ -44,36 +30,34 @@ class BeautyRequests extends React.Component {
                 <View style={styles.image}>
                 </View>
                 <View style={styles.content}>
-                    <Text style={styles.textType}>종 류  {this.state.canada}</Text>
-                    <View style={styles.type}>
-                        <Select
-                            width={250}
-                            height={40}
-                            ref="SELECT1"
-                            optionListRef={this._getOptionList.bind(this)}
-                            defaultValue="원하는 동물을 선택하세요 ..."
-                            onSelect={this._canada.bind(this)}>
-                            <Option>개</Option>
-                            <Option>고양이</Option>
-                            <Option>Manitoba</Option>
-                            <Option>New Brunswick</Option>
-                            <Option>Newfoundland and Labrador</Option>
-                            <Option>Northwest Territories</Option>
-                            <Option>Nova Scotia</Option>
-                            <Option>Nunavut</Option>
-                            <Option>Ontario</Option>
-                            <Option>Prince Edward Island</Option>
-                            <Option>Quebec</Option>
-                            <Option>Saskatchewan</Option>
-                            <Option>Yukon</Option>
-                        </Select>
-
-                        <OptionList ref="OPTIONLIST"/>
-                    </View>
-                    <View>
-                        <Text>종: </Text>
-                    </View>
+                    <TouchableHighlight onPress={this.openTypeModal.bind(this)} underlayColor='#c0c0c0'>
+                        <View style={styles.type}>
+                            <View style={{flex:6}}>
+                                <Text style={{marginLeft:10, marginTop:12}}>
+                                    원하는 동물을 선택하세요.
+                                </Text>
+                            </View>
+                            <View style={{flex:1, borderLeftWidth :1 ,borderLeftColor:'#a9a9a9'}}>
+                            </View>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={this.openTypeModal.bind(this)} underlayColor='#c0c0c0'>
+                        <View style={styles.type}>
+                            <View style={{flex:6}}>
+                                <Text style={{marginLeft:10, marginTop:12}}>
+                                    품종을 선택하세요.
+                                </Text>
+                            </View>
+                            <View style={{flex:1, borderLeftWidth :1 ,borderLeftColor:'#a9a9a9'}}>
+                            </View>
+                        </View>
+                    </TouchableHighlight>
                 </View>
+
+                <Modal style={[styles.modal, styles.typeModal]} position={"center"} ref={"typeModal"} >
+                    <Text>Modal centered</Text>
+                </Modal>
+
             </View>
         )
     }
@@ -106,16 +90,30 @@ var styles = StyleSheet.create({
     }
 
     ,content:{
-        flexDirection: 'column'
-    }
-
-    ,textType: {
-        left: 20
-        ,height: 20
+        marginTop: 5
+        ,flexDirection: 'column'
     }
 
     ,type: {
+        flexDirection: 'row'
+        ,flex:1
+        ,height:50
+        ,marginTop: 5
+        ,marginRight:10
+        ,marginLeft:10
+        ,borderWidth:1
+        ,borderColor:'#a9a9a9'
+        ,borderRadius: 10
+    }
+
+    ,modal: {
+        justifyContent: 'center',
         alignItems: 'center'
+    }
+
+    ,typeModal: {
+        height: 100
+        ,backgroundColor: "#3B5998"
     }
 })
 
