c3b128657a0a38166754cc09b0c1fe6d0f259636
JW-KIM <jw4005.kim@gmail.com>
2016-03-15 08:22:42 +0000

    /home/ubuntu/workspace/app/components/main/beauty/CompanyList.js

--- app/components/main/beauty/CompanyList.js
+++ app/components/main/beauty/CompanyList.js
@@ -28,24 +28,10 @@ class CompanyList extends React.Component {
 
     render(){
         return(
-            <View style={{  height:this.props.req.id == '' ? 0 : 200,
-                            flexDirection: 'column'}}>
-                <View style={{  height:this.props.req.id == '' ? 0 : 20
-                                ,marginTop:this.props.req.id == '' ? 0 : 10
-                                ,flexDirection: 'row'}}>
-                    <Image
-                        source={require('../../../../assets/img/ic_group.jpg')}
-                        style={{height:this.props.req.id == '' ? 0 : 20,
-                                width: 20, marginLeft: 15}}
-                    />
-                    <Text style={{  height:this.props.req.id == '' ? 0 : 20,
-                                    fontWeight:'bold', marginLeft: 5}}>입찰한 업체 리스트</Text>
-                </View>
-                <View style={{  height:this.props.req.id == '' ? 0 : 160
-                                ,borderWidth:this.props.req.id == '' ? 0 : 2
-                                ,marginTop: 2, marginRight:10, marginLeft:10
-                                , borderColor:'#a9a9a9', borderRadius: 5
-                                ,backgroundColor:'#E0E0E0'}}>
+            <View style={{flexDirection: 'column'}}>
+                <View style={{  borderWidth:this.props.req.id == '' ? 0 : 2
+                                ,borderColor:'#B3E5FC'
+                                ,backgroundColor:'#E1F5FE'}}>
                     <ListView
                         dataSource={this.state.dataSource}
                         renderRow={this.renderCompany.bind(this)}
@@ -61,7 +47,12 @@ class CompanyList extends React.Component {
             <TouchableWithoutFeedback>
                 <View style={{flexDirection:'row', marginTop:3}}>
                     <Text style={{flex:1,fontSize: 15, marginLeft:5,  color: '#101010'}}>{company.companyNm}</Text>
-                    <Text style={{flex:1, fontSize: 15,  color: '#101010'}}>{company.price}원</Text>
+                    <View style={{flex:1, flexDirection:'row', alignItems: 'center'}}>
+                        <Text style={{fontSize: 15,  color: '#101010'}}>{company.price}원</Text>
+                        <View style={{backgroundColor:'#F06292', marginLeft:10}}>
+                            <Text style={{fontSize: 10,  color: '#ffffff'}}>추가서비스</Text>
+                        </View>
+                    </View>
                     <Button containerStyle={{flex:0.5, height: 25, borderRadius:4, backgroundColor: '#616161', marginRight: 5}}
                             style={{fontSize: 15, color: 'white', height:25}}
                             onPress={this.props.openCompanyDetailModal}>
