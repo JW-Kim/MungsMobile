01e0979145dcca32368ee1ccc01984307c1ddf37
JW-KIM <jw4005.kim@gmail.com>
2016-03-17 05:44:11 +0000

    /home/ubuntu/workspace/app/components/main/CustomTabBar.js

---
+++ app/components/main/CustomTabBar.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Animated,
+} = React;
+
+const Icon = require('react-native-vector-icons/Ionicons');
+
+var styles = StyleSheet.create({
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+    backgroundColor:'#B2EBF2'
+  },
+  tabs: {
+    height: 45,
+    flexDirection: 'row',
+    paddingTop: 5,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: 'rgba(0,0,0,0.05)',
+  },
+  icon: {
+    position: 'absolute',
+    top: 0,
+    left: 50
+  },
+});
+
+var CustomTabBar = React.createClass({
+  selectedTabIcons: [],
+  unselectedTabIcons: [],
+
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array
+  },
+
+  renderTabOption(name, page) {
+    var isTabActive = this.props.activeTab === page;
+
+    var containerWidth = this.props.containerWidth;
+    var numberOfTabs = this.props.tabs.length;
+
+    console.log('containerWidth',containerWidth)
+    console.log('numberOfTabs',numberOfTabs)
+    console.log('width',(containerWidth/numberOfTabs) /2)
+
+    return (
+      <TouchableOpacity key={name} onPress={() => this.props.goToPage(page)} style={styles.tab}>
+        <Icon name={name} size={30} color='#00BCD4' style={styles.icon}
+              ref={(icon) => { this.selectedTabIcons[page] = icon }}/>
+        <Icon name={name} size={30} color='#FFFFFF' style={styles.icon}
+              ref={(icon) => { this.unselectedTabIcons[page] = icon }}/>
+      </TouchableOpacity>
+    );
+  },
+
+  componentDidMount() {
+    this.setAnimationValue({value: this.props.activeTab});
+    this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
+  },
+
+  setAnimationValue({value}) {
+    var currentPage = this.props.activeTab;
+
+    this.unselectedTabIcons.forEach((icon, i) => {
+      var iconRef = icon;
+
+      if (!icon.setNativeProps && icon !== null) {
+        iconRef = icon.refs.icon_image
+      }
+
+      if (value - i >= 0 && value - i <= 1) {
+        iconRef.setNativeProps({ style: {opacity: value - i} });
+      }
+      if (i - value >= 0 &&  i - value <= 1) {
+        iconRef.setNativeProps({ style: {opacity: i - value} });
+      }
+    });
+  },
+
+  render() {
+    var containerWidth = this.props.containerWidth;
+    var numberOfTabs = this.props.tabs.length;
+    var tabUnderlineStyle = {
+      position: 'absolute',
+      width: containerWidth / numberOfTabs,
+      height: 3,
+      backgroundColor: '#00BCD4',
+      bottom: 0,
+    };
+
+    var left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1], outputRange: [0, containerWidth / numberOfTabs]
+    });
+
+    return (
+      <View>
+        <View style={[styles.tabs, this.props.style, ]}>
+          {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+        </View>
+        <Animated.View style={[tabUnderlineStyle, {left}]} />
+      </View>
+    );
+  },
+});
+
+module.exports = CustomTabBar;
